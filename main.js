599ca7d829f876158ab365d84241c9cbeda6df3a
Grant Ralls <grantsralls@gmail.com>
2018-08-27 23:30:56 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
+
+//All the static things
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/post', (req, res) =>{
+  res.render('post')
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body.title);
+  res.redirect('/');
+});
+
+app.listen(3000, () => {
+  console.log('server started on port 3000');
+})
